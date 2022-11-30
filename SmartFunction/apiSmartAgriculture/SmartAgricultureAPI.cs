83b74833db51ceedd5e62c74eb8c4b3a5d82ad3d
David Cristóbal <david@juanked.es>
2022-11-30 13:54:36 +0100

    Añadido nuevo endpoint: getActuatorsFromPlantationID

--- SmartFunction/apiSmartAgriculture/SmartAgricultureAPI.cs
+++ SmartFunction/apiSmartAgriculture/SmartAgricultureAPI.cs
@@ -9,6 +9,9 @@ using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Threading.Tasks;
+using System.Configuration;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.EnvironmentVariables;
 
 namespace apiSmartAgriculture
 {
@@ -61,6 +64,32 @@ namespace apiSmartAgriculture
                 string query = $"EXEC getEdgeNodeConfig @edgeNodeID ='{id}'";
                 log.LogInformation(query);
                 connection.Open();
+                SqlCommand command = new(query, connection);
+                SqlDataAdapter da = new(command);
+                await Task.Run(() => da.Fill(dt));
+            }
+            catch (Exception e)
+            {
+                log.LogError(e.ToString());
+            }
+            if (dt.Rows.Count == 0)
+            {
+                return new NotFoundResult();
+            }
+            return new OkObjectResult(dt);
+        }
+
+        [FunctionName("GetActuatorsFromPlantationID")]
+        public static async Task<IActionResult> GetActuatorsFromPlantationID(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "plantation/{id}/actuators")] HttpRequest req, ILogger log, int id)
+        {
+            DataTable dt = new();
+            try
+            {
+                using SqlConnection connection = new(Environment.GetEnvironmentVariable("ConnectionString"));
+                string query = $"EXEC getActuatorsFromPlantationID @plantationID = '{id}'";
+                log.LogInformation(query);
+                connection.Open(); 
                 SqlCommand command = new(query, connection);
                 SqlDataAdapter da = new(command);
                 await Task.Run(() => da.Fill(dt));
